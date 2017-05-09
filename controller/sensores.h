fba62dd218891adafb5154e2747e7171496f7399
Unknown <carlos.cuencae@alumnos.upm.es>
2017-05-09 15:41:21 +0200

    removed sensors

--- controller/sensores.h
+++
@@ -1,7 +0,0 @@
-#ifndef sensors_H_
-#define sensors_H_
-
-void setupProximitySensors(int* pins, int nPins);
-float readProximitySensors();
-
-#endif
--- controller/sensors.h
+++
@@ -1,7 +0,0 @@
-#ifndef sensors_H_
-#define sensors_H_
-
-void setupProximitySensors(int* pins, int* angles, int nPins);
-float readProximitySensors();
-
-#endif
